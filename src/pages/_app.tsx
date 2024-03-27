70c99641b7a4036af80f2a4fa94cc8a4b6dd2953
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-03-27 10:46:17 -0300

    add a context in global page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { MathContext } from "@/context/formContext";
-
+import { MathInbursa } from "@/context/guardResfrommath";
 
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -11,9 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <ChakraProvider>
       {" "}
       <MathContext>
-        
+        <MathInbursa>
           <Component {...pageProps} />
-        
+        </MathInbursa>
       </MathContext>
     </ChakraProvider>
   );
