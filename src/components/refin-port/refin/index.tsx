f329998acb51878907b696226af5c186a41aa278
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-04-01 10:02:04 -0300

    chore: add pop up

--- src/components/refin-port/refin/index.tsx
+++ src/components/refin-port/refin/index.tsx
@@ -1,11 +1,13 @@
-import { Box, Flex, Table, TableCaption, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Flex, Table, TableCaption, Tbody, Td, Text, Th, Thead, Tr, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from "@chakra-ui/react";
 import Link from "next/link";
 import React, { useEffect, useState } from 'react';
 import { SlArrowDown, SlArrowRight } from "react-icons/sl";
 
-export const Port = ({color, data, sd,taxa }: any) => {
+export const Port = ({ color, data, sd, taxa }: any) => {
   const [ordenedList, setOrdenedList] = useState<any[]>([]);
-  
+  const [selectedItem, setSelectedItem] = useState<any>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   useEffect(() => {
     const formattedData: any[] = [];
     data.forEach((element: any) => {
@@ -24,10 +26,8 @@ export const Port = ({color, data, sd,taxa }: any) => {
 
     // Ordenar a lista pelo valor da taxa
     formattedData.sort((a, b) => a.tax - b.tax);
-    
+
     setOrdenedList(formattedData);
-    
-   
   }, [data]);
 
   const getRowColor = (nameBank: string) => {
@@ -44,38 +44,70 @@ export const Port = ({color, data, sd,taxa }: any) => {
     }
   };
 
+  const handleRowClick = (item: any) => {
+    setSelectedItem(item);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <Box mt={"20px"}>
-      <Text color={"#bbd5ed"} fontSize={["12px", "15px", "15px"]}></Text>
-      <Flex gap={"20px"} bg={color} p={"20px"} borderRadius={"8px"} overflowX="auto">
-        <Table variant="simple" fontSize={['10px', '12px', '12px', '13px', '15px']}>
-          <TableCaption color={"white"}>Portabilidade</TableCaption>
-          <Thead>
-            <Tr>
-              <Th color={'yellow'}>Bancos</Th>
-              <Th color={"yellow"}>Nova taxa</Th>
-              <Th color={"yellow"}>Nova parcela</Th>
-              <Th color={"yellow"}>Economia mensal cliente</Th>
-              <Th color={"yellow"}>Economia total período</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {ordenedList.map((item, index) => (
-              
-              <Tr key={index} style={{ backgroundColor: getRowColor(item.nameBank) }}>
-                <Td>{item.nameBank}</Td>
-                <Td>{item.tax}</Td>
-                <Td>{item.pmt}</Td>
-                <Td>{item.parcelaAtual - item.pmt}</Td>
-                <Td><Link href={`calculator/page/${item.nameBank}-${item.tax}-${item.parcelaAtual}-${sd}-${taxa}-${item.pmt}-${item.parcelaRestante}-${item.parcelaAtual - item.pmt}-${(item.parcelaAtual - item.pmt) * item.parcelaRestante}`}>{(item.parcelaAtual - item.pmt) * item.parcelaRestante} <SlArrowDown /></Link></Td>
-                
+    <>
+      <Box mt={"20px"}>
+        <Text color={"#bbd5ed"} fontSize={["12px", "15px", "15px"]}></Text>
+        <Flex gap={"20px"} bg={color} p={"20px"} borderRadius={"8px"} overflowX="auto">
+          <Table variant="simple" fontSize={['10px', '12px', '12px', '13px', '15px']}>
+            <TableCaption color={"white"}>Portabilidade</TableCaption>
+            <Thead>
+              <Tr>
+                <Th color={'yellow'}>Bancos</Th>
+                <Th color={"yellow"}>Nova taxa</Th>
+                <Th color={"yellow"}>Nova parcela</Th>
+                <Th color={"yellow"}>Economia mensal cliente</Th>
+                <Th color={"yellow"}>Economia total período</Th>
               </Tr>
-              
-            ))}
-          </Tbody>
-          
-        </Table>
-      </Flex>
-    </Box>
+            </Thead>
+            <Tbody>
+              {ordenedList.map((item, index) => (
+                <Tr key={index} style={{ backgroundColor: getRowColor(item.nameBank) }} onClick={() => handleRowClick(item)}>
+                  <Td>{item.nameBank}</Td>
+                  <Td>{item.tax}</Td>
+                  <Td>{item.pmt}</Td>
+                  <Td>{item.parcelaAtual - item.pmt}</Td>
+                  <Td>
+                    <div>
+                      {(item.parcelaAtual - item.pmt) * item.parcelaRestante} <SlArrowDown />
+                    </div>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Flex>
+      </Box>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} size="80%">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Detalhes do Item</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {selectedItem && (
+              <div>
+                <Flex gap={2}>Banco: <Text fontWeight={'600'}>{selectedItem.nameBank} </Text></Flex>
+                <Flex gap={2}>Nova Taxa: <Text fontWeight={'600'}>{selectedItem.tax}</Text></Flex>
+                <Flex gap={2}>Nova Parcela: <Text fontWeight={'600'}>{selectedItem.pmt}</Text></Flex>
+                <Flex gap={2}>Economia Mensal Cliente: <Text fontWeight={'600'}>{selectedItem.parcelaAtual - selectedItem.pmt}</Text></Flex>
+                <Flex gap={2}>Economia Total Período: <Text fontWeight={'600'}>{(selectedItem.parcelaAtual - selectedItem.pmt) * selectedItem.parcelaRestante}</Text></Flex>
+              </div>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            {/* Se necessário, adicione botões de ação no rodapé do modal */}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
